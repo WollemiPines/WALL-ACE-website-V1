773294d37972aa36c074a7840c2c7f29185f49e1
Chris Wallace <Chrisw1096@gmail.com>
2022-10-20 19:38:37 +1100

    productCard iterating without imputted values

--- client/src/pages/shop.jsx
+++ client/src/pages/shop.jsx
@@ -1,87 +1,73 @@
-
-import ProductCard from "../components/productCard";
 import React, { useEffect } from 'react';
-// import { useStoreContext } from '../../utils/GlobalState';
-// import { UPDATE_PRODUCTS } from '../../utils/actions';
-// import { useQuery } from '@apollo/client';
-// import { QUERY_PRODUCTS } from '../../utils/queries';
-// import { idbPromise } from '../../utils/helpers';
-// import spinner from '../../assets/spinner.gif';
+import { useStoreContext } from '../utils/GlobalState';
+import { UPDATE_PRODUCTS } from '../utils/actions';
+import { useQuery } from '@apollo/client';
+import { QUERY_PRODUCTS } from '../utils/queries';
+import { idbPromise } from '../utils/helpers';
+import ProductCard from "../components/productCard";
 
-// function ProductList() {
-//   const [state, dispatch] = useStoreContext();
 
-//   const { currentCategory } = state;
+function Shop() {
+  const [state, dispatch] = useStoreContext();
 
-//   const { loading, data } = useQuery(QUERY_PRODUCTS);
+  const { currentCategory } = state;
 
-//   useEffect(() => {
-//     if (data) {
+  const { loading, data } = useQuery(QUERY_PRODUCTS);
+
+  useEffect(() => {
+    if (data) {
       
-//       dispatch({
-//         type: UPDATE_PRODUCTS,
-//         products: data.products,
-//       });
-//       data.products.forEach((product) => {
-//         idbPromise('products', 'put', product);
-//       });
-//     } else if (!loading) {
-//       idbPromise('products', 'get').then((products) => {
-//         dispatch({
-//           type: UPDATE_PRODUCTS,
-//           products: products,
-//         });
-//       });
-//     }
-//   }, [data, loading, dispatch]);
-
-//   function filterProducts() {
-//     if (!currentCategory) {
-//       return state.products;
-//     }
-
-//     return state.products.filter(
-//       (product) => product.category._id === currentCategory
-//     );
-//   }
-
-//   return (
-//     <div className="my-2">
-//       <h2>Our Products:</h2>
-//       {state.products.length ? (
-//         <div className="flex-row">
-//           {filterProducts().map((product) => (
-//             <ProductCard
-//               key={product._id}
-//               _id={product._id}
-//               image={product.image}
-//               name={product.name}
-//               price={product.price}
-//               quantity={product.quantity}
-//             />
-//           ))}
-//         </div>
-//       ) : (
-//         <h3>You haven't added any products yet!</h3>
-//       )}
-//       {loading ? <img src={spinner} alt="loading" /> : null}
-//     </div>
-//   );
-// }
-
-// export default ProductList;
-
-
-
-class Shop extends React.Component {
-    
-  render() {
-      return (
-    <div>
-         <h1>Shop</h1>
-         <ProductCard/>
- </div>
-  )
+      dispatch({
+        type: UPDATE_PRODUCTS,
+        products: data.products,
+      });
+      data.products.forEach((product) => {
+        idbPromise('products', 'put', product);
+      });
+    } else if (!loading) {
+      idbPromise('products', 'get').then((products) => {
+        dispatch({
+          type: UPDATE_PRODUCTS,
+          products: products,
+        });
+      });
     }
-  };
-export default Shop
+  }, [data, loading, dispatch]);
+
+  function filterProducts() {
+    if (!currentCategory) {
+      return state.products;
+    }
+
+    return state.products.filter(
+      (product) => product.category._id === currentCategory
+    );
+  }
+
+  return (
+    <div className="my-2">
+      <h2>Our Products:</h2>
+      {state.products.length ? (
+        <div className="flex-row">
+          {filterProducts().map((product) => (
+            <ProductCard
+              key={product._id}
+              _id={product._id}
+              image={product.image}
+              name={product.name}
+              price={product.price}
+              quantity={product.quantity}
+            />
+          ))}
+        </div>
+      ) : (
+        <h3>You haven't added any products yet!</h3>
+      )}
+      {loading ? <div/> : null}
+    </div>
+  );
+}
+
+export default Shop;
+
+
