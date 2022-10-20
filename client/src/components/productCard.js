6ac2e7bd936c8f454c6099b25123555443414364
Chris Wallace <Chrisw1096@gmail.com>
2022-10-20 21:02:01 +1100

    loginsignup bttn and shop now bttn routed

--- client/src/components/productCard.js
+++ client/src/components/productCard.js
@@ -7,6 +7,11 @@ import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
+import { Link } from "react-router-dom";
+import { pluralize } from "../utils/helpers"
+import { useStoreContext } from "../utils/GlobalState";
+import { ADD_TO_CART, UPDATE_CART_QUANTITY } from "../utils/actions";
+import { idbPromise } from "../utils/helpers";
 
 const useStyles = makeStyles({
   root: {
@@ -14,45 +19,90 @@ const useStyles = makeStyles({
   },
 });
 
-//       <ProductCard
-// key={product._id}
-// _id={product._id}
-// image={product.image}
-// name={product.name}
-// price={product.price}
-// quantity={product.quantity}
-// />
 
-export default function ProductCard(key, _id, image, name, price, quantity) {
+function ProductCard(item) {
   const classes = useStyles();
+  const [state, dispatch] = useStoreContext();
+
+  const {
+    image,
+    name,
+    _id,
+    price,
+    quantity
+  } = item;
+
+  const { cart } = state
+
+  const addToCart = () => {
+    const itemInCart = cart.find((cartItem) => cartItem._id === _id)
+    if (itemInCart) {
+      dispatch({
+        type: UPDATE_CART_QUANTITY,
+        _id: _id,
+        purchaseQuantity: parseInt(itemInCart.purchaseQuantity) + 1
+      });
+      idbPromise('cart', 'put', {
+        ...itemInCart,
+        purchaseQuantity: parseInt(itemInCart.purchaseQuantity) + 1
+      });
+    } else {
+      dispatch({
+        type: ADD_TO_CART,
+        product: { ...item, purchaseQuantity: 1 }
+      });
+      idbPromise('cart', 'put', { ...item, purchaseQuantity: 1 });
+    }
+  }
 
   return (
-    <Card className={classes.root}>
-      <CardActionArea>
-        <CardMedia
-          component="img"
-          alt={name}
-          height="140"
-          image={image}
-          title={name}
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="h2">
-            {name}
-          </Typography>
-          <Typography variant="body2" color="textSecondary" component="p">
-            {price}
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-      <CardActions>
-        <Button size="small" color="primary">
-          Add to Cart
-        </Button>
-        <Button size="small" color="primary">
-          Remove From Cart
-        </Button>
-      </CardActions>
-    </Card>
+    // <div className="card px-1 py-1">
+    //   <Link to={`/products/${_id}`}>
+    //     <img
+    //       alt={name}
+    //       src={`/images/${image}`}
+    //     />
+    //     <p>{name}</p>
+    //   </Link>
+    //   <div>
+    //     <div>{quantity} {pluralize("item", quantity)} in stock</div>
+    //     <span>${price}</span>
+    //   </div>
+    //   <button onClick={addToCart}>Add to cart</button>
+    // </div>
+
+
+
+  <Card className={classes.root}>
+    <Link to={`/products/${_id}`}>
+  <CardActionArea>
+    <CardMedia
+      component="img"
+      alt={name}
+      height="140"
+      image={image}
+      title={name}
+    />
+    <CardContent>
+      <Typography gutterBottom variant="h5" component="h2">
+        {name}
+      </Typography>
+      <Typography variant="body2" color="textSecondary" component="p">
+        {price}
+      </Typography>
+    </CardContent>
+  </CardActionArea>
+  </Link>
+  <CardActions>
+    <Button onClick={addToCart} size="small" color="primary">
+      Add to Cart
+    </Button>
+    <Button size="small" color="primary">
+      Remove From Cart
+    </Button>
+  </CardActions>
+</Card>
   );
 }
+
+export default ProductCard
