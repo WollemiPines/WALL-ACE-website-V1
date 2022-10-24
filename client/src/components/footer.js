c5017cbbbe216d005afe0137b4f3fce88c87352c
Chris Wallace <Chrisw1096@gmail.com>
2022-10-24 20:33:18 +1100

    updated deployment

--- client/src/components/footer.js
+++ client/src/components/footer.js
@@ -1,13 +1,11 @@
 import React, { useState } from 'react';
-import FbIcon from './icon_fb';
-import InstaIcon from './icon_insta';
+
 const Footer = () =>{
     return (
         <footer>
         <div className="container">
          <p className="footer-text">Copyright Wall Ace 2022</p>
-         {/* < InstaIcon/> */}
-         {/* < FbIcon/> */}
+
         </div>
       </footer>
     )
