8cb791f23c3366ee7494ddd8fe4a6be9b9e5942a
Chris Wallace <Chrisw1096@gmail.com>
2022-10-24 15:23:22 +1100

    cart

--- client/src/components/icon_cart.js
+++ client/src/components/icon_cart.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import { Router, Link } from 'react-router-dom';
+import { TOGGLE_CART } from '../utils/actions';
+import { useStoreContext } from '../utils/GlobalState';
 
-export default function Cart() {
+
+export default function CartIcon() {
+  const [ dispatch] = useStoreContext();
+  function toggleCart() {
+    dispatch({ type: TOGGLE_CART });
+  }
+
   return (
 
       <div className="cart_icon_container">
       
          <Link to="/cart">  
-         <img className='cart-icon' alt="Cart" src='/images/icons/cart.jpg'/>
+         <img  className='cart-icon' alt="Cart" src='/images/icons/cart.jpg'/>
          </Link>
   
       </div>
