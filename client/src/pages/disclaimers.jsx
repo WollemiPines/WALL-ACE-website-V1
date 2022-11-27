3126066026e72af120dfcf4346256a9d6cfc6ea0
Chris Wallace <Chrisw1096@gmail.com>
2022-11-27 16:01:39 +1100

    disclaimers

--- client/src/pages/disclaimers.jsx
+++ client/src/pages/disclaimers.jsx
@@ -5,7 +5,7 @@ class Disclaimers extends React.Component {
   render() {
       return (
     <div className='disclaimers-container'>
-         <h1>Disclaimers</h1>
+         <h1>Warning! Activities of any kind can be dangerous!</h1>
          <p>Comming Soon</p>
  </div>
   )
