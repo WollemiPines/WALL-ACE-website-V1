232f4640b040047e2346435e67f3e7d83fdb1896
Chris Wallace <Chrisw1096@gmail.com>
2022-11-27 16:04:18 +1100

    disclaimers

--- client/src/pages/disclaimers.jsx
+++ client/src/pages/disclaimers.jsx
@@ -5,8 +5,24 @@ class Disclaimers extends React.Component {
   render() {
       return (
     <div className='disclaimers-container'>
+         <div classname='disclaimer-red'> 
          <h1>Warning! Activities of any kind can be dangerous!</h1>
-         <p>Comming Soon</p>
+         <p>Wall Ace, to the best of its abilities, designs and manufactures high-quality outdoor goods. It is impossible to ensure the safety and security of its products short or long term. It is the responsibility of the product user to carefully inspect and assess the integrity and quality of their gear before EVERY use and at regular intervals between uses. Failure to do so can result in severe injury or death.
+Wall Ace accepts no liability for harm to person or property from the use or misuse of its products. 
+No product is rated or intended to hold a human in any way, implied or otherwsie
+If a product is determined or perceived to be, including not limited to, damaged, excessively worn, used beyond its Working Load Limit (WLL), in-service exceeding the recommended lifespan, malfunctioning, visually inconsistent, contaminated or in any way unsafe, it is to be retired and destroyed immediately.
+</p>
+</div>
+<div classname='disclaimer-body'>
+<h1>
+Disclaimer Continued...
+</h1>
+<p>
+This is a collection of instruction manuals associated with Wall Ace Products. Wall Ace reserves the right to, at any point in time and for whatever reason, change, alter, add or remove information regarding the correct use of its products. In the circumstances where safety is critical, Wall Ace will make the appropriate effort to alert the relevant product owners however at all times it remains the responsibility of the individual product user to familiarise themselves with the most up to date information possible BEFORE use and during significant intervals of no use. 
+  No product is rated in any capacity, implied or otherwise, to hold human life. Any values are given indicating MBS (Maximum Breaking Strength) or WLL (Working Load Limit) are purely for the use of inanimate loads, pertaining no compensatable value. Using the product for anything of real value or in any circumstance that could lead to injury or death is not condoned or recommended by Wall Ace.
+ Failure to properly remove and destroy faulty equipment can lead to severe injury or death, the purchaser accepts this responsibility upon receiving of goods, paid or otherwise. In the case of resale to third parties, it Is the responsibility of the resaler to provide any and all safety information related to the product.
+</p>
+</div>
  </div>
   )
     }
